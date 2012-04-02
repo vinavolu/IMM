90ef07844ed7c8c6e1a6006efc6c32e8003248fc
Vasu Reddy <vasu@Vasu-Reddys-MacBook-Pro.local>
2012-04-01 21:35:19 -0400

    * initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imm::Application.config.secret_token = 'd6787e876cfc3b68324f0032620922403a8b03bdacb012367a820fc918b41ee88e95301607502745dde968142196a2cf85e1a6cd5a3e40d01e4435cc4805e8a8'
